eaddd331c02a143f79dc14a6f86d640f742a871b
chandrahas <chandrahussy6@gmail.com>
2018-05-29 20:51:24 +0530

    my first selenium code into the git repository

---
+++ src/main/java/maventest/maventest/Testng.java
@@ -0,0 +1,15 @@
+package maventest.maventest;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
+public class Testng {
+@Test
+public void m1(){
+	int i=20;
+	int j=30;
+	System.out.println(i+j);
+	//assert.assertEquals(true, false);
+}
+}
